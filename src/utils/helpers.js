21380bbe946de78dd9f307b65a24b1c292ff2798
doge-is-dope <maple3312936@outlook.com>
2020-10-21 00:01:54 +0800

    chore: add required files

---
+++ src/utils/helpers.js
@@ -0,0 +1,23 @@
+/**
+ * Return all of the decks along with their titles, questions, and answers.
+ */
+export const getDecks = () => {};
+
+/**
+ * Take in a single id argument and return the deck associated with that id.
+ * @param {string} id
+ */
+export const getDeck = (id) => {};
+
+/**
+ * Take in a single title argument and add it to the decks.
+ * @param {string} title
+ */
+export const saveDeckTitle = (title) => {};
+
+/**
+ * Take in two arguments, id and card, and will add the card to the list of questions for the deck with the associated title.
+ * @param {string} id
+ * @param {Object} card
+ */
+export const addCardToDeck = (id, card) => {};
